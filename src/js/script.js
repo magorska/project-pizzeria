bec554a659fafb82f426673ba9355f2b27a0521c
Marta Gorska <magorska.95@gmail.com>
2021-12-15 17:26:23 +0100

    Add optionImage

--- src/js/script.js
+++ src/js/script.js
@@ -89,6 +89,7 @@
       thisProduct.formInputs = thisProduct.form.querySelectorAll(select.all.formInputs);
       thisProduct.cartButton = thisProduct.element.querySelector(select.menuProduct.cartButton);
       thisProduct.priceElem = thisProduct.element.querySelector(select.menuProduct.priceElem);
+      thisProduct.imageWrapper = thisProduct.element.querySelector(select.menuProduct.imageWrapper);
     }
 
     initAccordion() {
@@ -159,27 +160,36 @@
           const option = param.options[optionId];
           console.log(optionId, option);
 
+          const optionSelected = formData[paramId] && formData[paramId].includes(optionId);
+          const optionImage = thisProduct.imageWrapper.querySelector('.' + paramId + '-' + optionId);
+
           // check if there is param with a name of paramId in formData and if it includes optionId
-          if (formData[paramId] && formData[paramId].includes(optionId)) {
+          if (optionSelected) {
             // check if the option is not default
             if (option.default !== true) {
               // add option price to price variable
               price += option.price;
             }
-          } else {
             // check if the option is default
-            if (option.default == true) {
-              // reduce price variable
-              price -= option.price;
+          } else if (option.default == true) {
+            // reduce price variable
+            price -= option.price;
+          }
+
+          if (optionImage) {
+            if (optionSelected) {
+              optionImage.classList.add(classNames.menuProduct.imageVisible);
+            } else if (!optionSelected) {
+              optionImage.classList.remove(classNames.menuProduct.imageVisible);
             }
           }
         }
       }
-
       // update calculated price in the HTML
       thisProduct.priceElem.innerHTML = price;
     }
   }
+
 
   const app = {
     initMenu: function () {
