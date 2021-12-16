a4b4f03ed8416a5cfa57753189dd3468ac0580b1
Marta Gorska <magorska.95@gmail.com>
2021-12-16 11:44:50 +0100

    Add Cart class

--- src/js/script.js
+++ src/js/script.js
@@ -289,6 +289,35 @@
     }
   }
 
+  class Cart {
+    constructor(element) {
+
+      const thisCart = this;
+
+      thisCart.products = [];
+      thisCart.getElements(element);
+      thisCart.initActions();
+
+      console.log('new Cart: ', thisCart);
+    }
+
+    getElements(element) {
+      const thisCart = this;
+      thisCart.dom = {};
+      thisCart.dom.wrapper = element;
+
+      thisCart.dom.toggleTrigger = thisCart.dom.wrapper.querySelector(select.cart.toggleTrigger);
+    }
+
+    initActions() {
+      const thisCart = this;
+
+      thisCart.dom.toggleTrigger.addEventListener(`click`, function () {
+        thisCart.dom.wrapper.classList.toggle(classNames.cart.wrapperActive);
+      });
+    }
+  }
+
 
   const app = {
     initMenu: function () {
@@ -303,6 +332,12 @@
       const thisApp = this;
       thisApp.data = dataSource;
     },
+    initCart: function () {
+      const thisApp = this;
+
+      const cartElem = document.querySelector(select.containerOf.cart);
+      thisApp.cart = new Cart(cartElem);
+    },
     init: function () {
       const thisApp = this;
       console.log('*** App starting ***');
@@ -313,6 +348,7 @@
 
       thisApp.initData();
       thisApp.initMenu();
+      thisApp.initCart();
     },
   };
 
